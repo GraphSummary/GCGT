b8304f0df59db0c93e9d77add8920a0df8d8b3b1
yusong <1963408085@qq.com>
2021-03-25 11:16:23 +0800

    clean delta_pagerank_.cpp

--- virtual_node_miner/delta_pagerank_.cpp
+++ virtual_node_miner/delta_pagerank_.cpp
@@ -23,7 +23,7 @@ struct Page
 class DeltaPageRank{
 public:
     //把每个页面的信息（outPage，value，oldDelta，recvDelta）存到向量里
-    void getPagesVec(string file)
+    void getPagesVec(const std::string &file)
     {
         ifstream inFile(file);
         if(!inFile){
@@ -53,7 +53,7 @@ public:
         inFile.close();
     }
     // 读取所有点
-    int read_nodes_num(string compress_vertex_path) {
+    int read_nodes_num(const std::string &compress_vertex_path) {
         ifstream inFile(compress_vertex_path);
         if(!inFile){
             cout << "open file failed. " << compress_vertex_path << endl;
@@ -66,26 +66,33 @@ public:
         return _raw_num_node;
     }
 
-    int run(string old_dege_file, string filename){
-        // 初始化计时器 
-        timer_start(true);
-        string v_path = "./out/" + filename + ".v"; // 压缩图点文件，包含超点和源顶
+    void write_result(){
+        string outPath = "./out/pr_delta_pre.txt";
+        cout << "out path: " << outPath << endl;
+        timer_next("write_graph");
+        ofstream fout(outPath);
+        for(int i = 0; i < vertex_num; i++){
+            Page& page = pages[i];
+            fout << vertex_reverse_map[i] << ' ' << page.value << endl;
+        }
+        fout.close();
+    }
 
-        timer_next("load_graph");
-        int nodes_numble = read_nodes_num(v_path);
+    void load_data(const std::string &base_e, const std::string &com_base_v){
+        int nodes_numble = read_nodes_num(com_base_v);
         pages = new Page[nodes_numble + 1]; // 申请node数组
-        getPagesVec(old_dege_file);   // 边文件路径
-        
+        getPagesVec(base_e);   // 边文件路径
+    }
+
+    int run(){
         int step = 0; //统计迭代几轮
         float delta_sum = 0;
-
         // 开始迭代
-        timer_next("compute_graph");
+        computer_num++;
         while(1)
         {
-            int shouldStop = 0; //根据oldPR与newPR的差值 判断是否停止迭代
             delta_sum = 0;
-            for(int i = 0; i < nodes_numble; i++){
+            for(int i = 0; i < vertex_num; i++){
                 Page& page = pages[i];
                 int outDegree = page.outPage.size();
                 double tmpDelta = page.oldDelta / outDegree * d;
@@ -97,7 +104,7 @@ public:
                 }
             }
 
-            for(int i = 0; i < nodes_numble; i++){
+            for(int i = 0; i < vertex_num; i++){
                 Page& page = pages[i];
                 page.value += page.recvDelta;
                 delta_sum += page.recvDelta;
@@ -115,19 +122,9 @@ public:
         // 将运行次数写入文件
         string resultPath = "./out/result.txt";
         ofstream fout_1(resultPath, ios::app);
-        fout_1 << "normal_graph_step:" << step << endl;
+        fout_1 << "normal_graph_" << computer_num << "_step:" << step << endl;
         fout_1.close();
 
-        string outPath = "./out/pr_delta_pre.txt";
-        cout << "out path: " << outPath << endl;
-        timer_next("write_graph");
-        ofstream fout(outPath);
-        for(int i = 0; i < nodes_numble; i++){
-            Page& page = pages[i];
-            fout << vertex_reverse_map[i] << ' ' << page.value << endl;
-        }
-        fout.close();
-        timer_end(true, "-nor_graph");
         return 0;
     }
     // ~DeltaPageRank(){
@@ -138,16 +135,39 @@ public:
     unordered_map<int, int> vertex_map; //原id: 新id
     unordered_map<int, int> vertex_reverse_map; // 新id: 原id
     int vertex_num = 0; // 所有点的个数
+    static int computer_num;
 };
+
+int DeltaPageRank::computer_num = 0;
 
 int main(int argc, char const *argv[])
 {
     // 运行命令: ./a.out 0.0000001 ./input/p2p-31_new.e p2p-31
+    // ./a.out $CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${updated_e} ${com_updated_v}
+    if (argc != 6) {
+        printf("incorrect arguments.\n");
+        printf("$CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${updated_e} ${com_updated_v}\n");
+        abort();
+    }
+
     threshold = atof(argv[1]);
+    string base_e(argv[2]);
+    string com_base_v(argv[3]);
+    string updated_e(argv[4]);
+    string com_updated_v(argv[5]);
+    // 初始化计时器 
+    timer_start(true);
+
+    // 原图计算
     DeltaPageRank deltaPageRank = DeltaPageRank();
-    string edge_new_path(argv[2]);
-    string filename(argv[3]);
-    deltaPageRank.run(edge_new_path, filename);
+    timer_next("load_graph");
+    deltaPageRank.load_data(base_e, com_base_v);
+    timer_next("compute_graph");
+    deltaPageRank.run();
+    timer_next("write_result");
+    deltaPageRank.write_result();
+
+    timer_end(true, "-nor_graph");
     return 0;
 }
 
