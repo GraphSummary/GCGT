80a05ccea4c9226a37cac0fd8b9adb417100534a
yusong <1963408085@qq.com>
2021-04-14 21:25:10 +0800

    sh

--- virtual_node_miner/run.sh
+++ virtual_node_miner/run.sh
@@ -4,7 +4,7 @@ make
 path=/home/yusong/dataset/web-uk-2005  # 原图边文件
 # path=/home/yusong/dataset/test  # 原图边文件
 DATASETNAME=web-uk-2005   # 文件名
-INC_RATE=0.01
+INC_RATE=0.05
 # 预处理后文件
 base_e="${path}/${DATASETNAME}_base.e"
 update_e="${path}/${DATASETNAME}_update_${INC_RATE}.e"
@@ -16,8 +16,8 @@ com_updated_e="${path}/${DATASETNAME}_updated_com.e"
 com_updated_v="${path}/${DATASETNAME}_updated_com.v"
 # 参数
 CONVERGENCE_THRESHOLD=0.000001 # 收敛阈值
-CLUSTER_THRESHOLD=30 # 压缩参数
-VIRTUAL_THRESHOLD=1
+CLUSTER_THRESHOLD=50 # 压缩参数
+VIRTUAL_THRESHOLD=20
 # 写入文件
 destdir="./out/result.txt"
 echo "path:$path" > "${destdir}"        # 截断文件后，追加文本
@@ -33,23 +33,23 @@ echo -e "压缩完成...\n"
 
 # 压缩图计算
 g++ delta_pagerank_sum.cpp
-# ./a.out $CONVERGENCE_THRESHOLD ${com_base_e} ${com_base_v} ${com_updated_e} ${com_updated_v}
+./a.out $CONVERGENCE_THRESHOLD ${com_base_e} ${com_base_v} ${com_updated_e} ${com_updated_v}
 # ./a.out $CONVERGENCE_THRESHOLD ${com_updated_e} ${com_updated_v} ${com_updated_e} ${com_updated_v} # 全是修改后的
-./a.out $CONVERGENCE_THRESHOLD ${com_base_e} ${com_base_v} ${com_base_e} ${com_base_v} # 全是修改前的
+# ./a.out $CONVERGENCE_THRESHOLD ${com_base_e} ${com_base_v} ${com_base_e} ${com_base_v} # 全是修改前的
 # ./a.out 0.000001 /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.e /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.v  /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.e /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.v
 echo -e "压缩图计算完成...\n"
 
 # 原图计算
 g++ delta_pagerank_.cpp 
-# ./a.out $CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${updated_e} ${com_updated_v}
+./a.out $CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${updated_e} ${com_updated_v}
 # ./a.out $CONVERGENCE_THRESHOLD ${updated_e} ${com_updated_v} ${updated_e} ${com_updated_v} # 全是修改后的
-./a.out $CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${base_e} ${com_base_v}  # 全是修改前的
+# ./a.out $CONVERGENCE_THRESHOLD ${base_e} ${com_base_v} ${base_e} ${com_base_v}  # 全是修改前的
 # ./a.out 0.000001 /home/yusong/dataset/web-uk-2005/web-uk-2005_updated.e /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.v  /home/yusong/dataset/web-uk-2005/web-uk-2005_updated.e /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.v
 # ./a.out 0.000001 /home/yusong/dataset/web-uk-2005/web-uk-2005_base.e /home/yusong/dataset/web-uk-2005/web-uk-2005_base_com.v  /home/yusong/dataset/web-uk-2005/web-uk-2005_updated.e /home/yusong/dataset/web-uk-2005/web-uk-2005_updated_com.v
 echo -e "原图计算完成...\n"
 
 # 比较两种计算结果
-/usr/bin/python3 ./tools/compare_result_pagerank.py
+/usr/bin/python3 ./tools/compare_result.py ./out/pr_delta_pre.txt ./out/pr_delta_sum_com.txt
 echo -e "计算结果比较完成...\n"
 
 # 写入excel
