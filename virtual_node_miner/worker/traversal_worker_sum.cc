328a7df4be499576b3d46cf6c8ca23a9bf47597d
yusong <1963408085@qq.com>
2021-04-15 20:31:18 +0800

    fix

--- virtual_node_miner/worker/traversal_worker_sum.cc
+++ virtual_node_miner/worker/traversal_worker_sum.cc
@@ -44,10 +44,9 @@ public:
         value_t value;
         vector<std::pair<vertex_t, value_t>> data;
         for(vertex_t i = 0; i < this->nodes_num; i++){
-            app_->init_c(this->vertex_reverse_map[i], delta, data);
-            this->nodes[i].recvDelta = delta;
-            app_->init_v(i, value, data);
-            this->nodes[i].value = value;
+            app_->init_c(this->vertex_reverse_map[i], this->nodes[i].recvDelta, data);
+            this->nodes[i].oldDelta = app_->default_v();
+            app_->init_v(i, this->nodes[i].value, data);
         }
     }
 
@@ -71,6 +70,9 @@ public:
         // print_result();
 
         vertex_t step = 0;
+        vertex_t contain_cnt = 0;
+        vertex_t node_send_cnt = 0;
+        vertex_t super_send_cnt = 0;
         value_t delta_sum = 0;
         while(true){
             delta_sum = 0;
@@ -80,44 +82,76 @@ public:
                 value_t old_value = node.value;
                 app_->accumulate(node.value, node.recvDelta); // delat -> value
                 delta_sum += std::fabs(old_value - node.value); // 负的累加在一起会抵消，导致提前小于阈值
+                // app_->accumulate(node.oldDelta, node.recvDelta); // updata delat
                 node.oldDelta = node.recvDelta;
                 node.recvDelta = app_->default_v();
             }
             // send
             for(vertex_t i = 0; i < this->nodes_num; i++){
                 Node<vertex_t, value_t>& node = this->nodes[i];
+                if(this->Fc[i] != this->Fc_default_value){ // 测试：仅仅用来统计内点次数
+                    contain_cnt++;
+                }
                 if(node.oldDelta == app_->default_v()){
                     continue;
                 }
-                for(auto& edge : node.out_adj){ // i -> adj
-                    value_t& recvDelta = this->nodes[edge.first].recvDelta; // adj's recvDelta
-                    value_t sendDelta; // i's 
-                    app_->g_func(node.oldDelta, edge.second, sendDelta);
-                    app_->accumulate(recvDelta, sendDelta); // sendDelta -> recvDelta
+                if(this->Fc[i] == i){
+                    // 超点send
+                    ExpandData<vertex_t, value_t>& supernode = this->expand_data[this->Fc_map[i]];
+                    Node<vertex_t, value_t>& node = this->nodes[supernode.id];
+                    if(node.oldDelta == app_->default_v()){
+                        continue;
+                    }
+                    for(auto& edge : supernode.edges){ // i -> adj
+                        value_t& recvDelta = this->nodes[edge.first].recvDelta; // adj's recvDelta
+                        value_t sendDelta; // i's 
+                        app_->g_func(node.oldDelta, edge.second, sendDelta);
+                        app_->accumulate(recvDelta, sendDelta); // sendDelta -> recvDelta
+                        super_send_cnt++;
+                    }
                 }
-            }
-            // 超点发送
-            for(vertex_t i = 0; i < this->supernodes_num; i++){
-                ExpandData<vertex_t, value_t>& supernode = this->expand_data[i];
-                Node<vertex_t, value_t>& node = this->nodes[supernode.id];
-                if(node.oldDelta == app_->default_v()){
+                else if(this->Fc[i] == this->Fc_default_value){
+                    // 普通点send
+                    for(auto& edge : node.out_adj){ // i -> adj
+                        value_t& recvDelta = this->nodes[edge.first].recvDelta; // adj's recvDelta
+                        value_t sendDelta; // i's 
+                        app_->g_func(node.oldDelta, edge.second, sendDelta);
+                        app_->accumulate(recvDelta, sendDelta); // sendDelta -> recvDelta
+                        node_send_cnt++;
+                    }
+                }
+                else{
                     continue;
                 }
-                for(auto& edge : supernode.edges){ // i -> adj
-                    value_t& recvDelta = this->nodes[edge.first].recvDelta; // adj's recvDelta
-                    value_t sendDelta; // i's 
-                    app_->g_func(node.oldDelta, edge.second, sendDelta);
-                    app_->accumulate(recvDelta, sendDelta); // sendDelta -> recvDelta
-                }
+                node.oldDelta = app_->default_v(); // delta发完需要清空
             }
+            // for(vertex_t i = 0; i < this->supernodes_num; i++){
+            //     ExpandData<vertex_t, value_t>& supernode = this->expand_data[i];
+            //     Node<vertex_t, value_t>& node = this->nodes[supernode.id];
+            //     if(node.oldDelta == app_->default_v()){
+            //         continue;
+            //     }
+            //     for(auto& edge : supernode.edges){ // i -> adj
+            //         value_t& recvDelta = this->nodes[edge.first].recvDelta; // adj's recvDelta
+            //         value_t sendDelta; // i's 
+            //         app_->g_func(node.oldDelta, edge.second, sendDelta);
+            //         app_->accumulate(recvDelta, sendDelta); // sendDelta -> recvDelta
+            //         super_send_cnt++;
+            //     }
+            //     node.oldDelta = app_->default_v();
+            // }
             // print_result();
             step++;
             LOG(INFO) << "step=" << step << " delta_sum=" << delta_sum;
-            if(delta_sum < FLAGS_convergence_threshold || step > 90){
+            if(delta_sum < FLAGS_convergence_threshold || step > 1000){
                 break;
             }
         }        
-
+        LOG(INFO) << "app convergence step=" << step << " delta_sum=" << delta_sum << " g_cnt=" << app_->g_cnt <<
+        " f_cnt=" << app_->f_cnt;
+        LOG(INFO) << "contain_cnt=" << contain_cnt;
+        LOG(INFO) << "node_send_cnt=" << node_send_cnt << " super_send_cnt=" << super_send_cnt << " +=" << (node_send_cnt+super_send_cnt);
+        printf("node_send_cnt=%d= super_send_cnt=%d\n", node_send_cnt, super_send_cnt);
     }
 
 protected:
@@ -127,10 +161,10 @@ protected:
 
 
 int main(int argc,char **argv) {
-    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=../input/test_data_sssp_pattern_2.e -shortestpath_source=0 -output=../out/sssp_result_sum
-    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/code/a_autoInc/AutoInc/dataset/p2p-31.e -shortestpath_source=1 -output=../out/sssp_result
-    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/dataset/p2p-Gnutella31/p2p-Gnutella31_weighted.e -shortestpath_source=1 -output=../out/sssp_result
-    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/dataset/inf-roadNet-CA/inf-roadNet-CA_weighted.e -shortestpath_source=1 -output=../out/sssp_result
+    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=../input/test_data_sssp_pattern_base.e -shortestpath_source=0 -output=../out/sssp_result_sum
+    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/code/a_autoInc/AutoInc/dataset/p2p-31_base.e -shortestpath_source=0 -output=../out/sssp_result_sum
+    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/dataset/p2p-Gnutella31/p2p-Gnutella31_weighted_base.e -shortestpath_source=0 -output=../out/sssp_result_sum
+    // g++ traversal_worker_sum.cc -lgflags -lglog && ./a.out -base_edge=/home/yusong/dataset/inf-roadNet-CA/inf-roadNet-CA_weighted_base.e -shortestpath_source=0 -output=../out/sssp_result_sum
 
     google::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
@@ -143,11 +177,13 @@ int main(int argc,char **argv) {
     TraversalWorkerSum<int, float> worker = TraversalWorkerSum<int, float>();
     timer_next("load_graph");
     worker.load(base_edge);
-    timer_next("find pattern");
+    timer_next("find_pattern");
     worker.start_find();
+    timer_next("write_pattern");
+    worker.write_supernode("../out/a_pattern");
     timer_next("compute");
     worker.start();
-    timer_next("write result");
+    timer_next("write_result");
     worker.write_result(output);
     timer_end(true, "TraversalWorkerSum"+base_edge, "../out/traversal_run_time");
 
