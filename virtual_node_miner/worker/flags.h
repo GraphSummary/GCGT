d3f6c6a2f67039d4a7c52a72de87eddd44090095
yusong <1963408085@qq.com>
2021-04-26 20:53:05 +0800

    flag

--- virtual_node_miner/worker/flags.h
+++ virtual_node_miner/worker/flags.h
@@ -1,6 +1,12 @@
+#ifndef WORKER_FLAGS_H_
+#define WORKER_FLAGS_H_
+
 DEFINE_double(convergence_threshold, 0.000001, "iterate convergence threshold");
 DEFINE_double(delta_step_threshold, 999999999, "delta step threshould");
 DEFINE_string(base_edge, "", "old edge file");
 DEFINE_string(output, "", "result output file");
 DEFINE_string(result_analyse, "", "result analyse file");
 DEFINE_uint32(max_iterater_num, 10000, "max_iterater_num");
+DEFINE_int64(shortestpath_source, -1, "source");
+
+#endif  // WORKER_FLAGS_H_
