6da7927b0d1285fc6b670355b96cc71ff56b6920
yusong <1963408085@qq.com>
2021-05-09 18:59:39 +0800

    flags

--- virtual_node_miner/worker/flags.h
+++ virtual_node_miner/worker/flags.h
@@ -6,7 +6,9 @@ DEFINE_double(delta_step_threshold, 999999999, "delta step threshould");
 DEFINE_string(base_edge, "", "old edge file");
 DEFINE_string(output, "", "result output file");
 DEFINE_string(result_analyse, "", "result analyse file");
-DEFINE_uint32(max_iterater_num, 10000, "max_iterater_num");
-DEFINE_int64(shortestpath_source, -1, "source");
+DEFINE_uint32(max_iterater_num, 1000, "max_iterater_num");
+DEFINE_int64(sssp_source, -1, "source");
+DEFINE_int64(php_source, -1, "source");
+DEFINE_string(app, "", "application name");
 
 #endif  // WORKER_FLAGS_H_
